98d11e335d71bdd6431ff87c7288483de04fe069
Olamide Atitebi <olamideumarq@gmail.com>
2022-04-05 07:14:20 +0100

    Edit README file

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const password = document.getElementById("password");
 const email = document.getElementById("email");
 // add an event listener to the form for the submit event
 singupForm.addEventListener("submit", submitForm);
+
 singupForm.addEventListener("input", (event) => {
     event.target.parentElement.classList.remove("invalid");
 });
@@ -12,15 +13,12 @@ singupForm.addEventListener("input", (event) => {
 // handle form submission
 function submitForm(event) {
     event.preventDefault();
-    console.log("hello");
     validateInput(lastName);
     validateInput(firstName);
     validateEmail(email);
     validatePassword(password);
     // if there is no invalid input, allow form to get submitted
-
     if (!document.getElementsByClassName("invalid").length) {
-        console.log("world");
         singupForm.removeEventListener("submit", submitForm);
         singupForm.submit();
     }
@@ -35,12 +33,12 @@ function validateInput(element) {
     element.parentElement.classList.remove("invalid");
 }
 
+// validate email
 function validateEmail(element) {
     const re =
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!re.test(element.value)) {
         element.parentElement.classList.add("invalid");
-        console.log(element.parentElement);
         element.nextElementSibling.innerText = element.value
             ? "Looks like this is not an email"
             : "Email cannot be empty";
